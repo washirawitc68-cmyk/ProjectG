4ae94380b7105db5f21dcb9df5b64820c79a08c8
SiMz <washirawit.c68@psru.ac.th>
2026-09-17 19:12:52 +0700

    Auto Update

--- app/src/main/java/com/example/project/BaseSolutionDialog.java
+++ app/src/main/java/com/example/project/BaseSolutionDialog.java
@@ -1,4 +1,24 @@
 package com.example.project;
 
-public class BaseSolutionDialog {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class BaseSolutionDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_base_solution_dialog);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
